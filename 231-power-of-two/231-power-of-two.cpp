b77997f88114c55d989c9b09fc76938792f2f7ab
Anica Singh <62740213+Anica8@users.noreply.github.com>
2022-01-26 13:59:25 +0530

    Time: 4 ms (33.52%), Space: 5.8 MB (87.88%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPwer(int n){
+        int count=0;
+        while(n!=0){
+            if(n&1==1)
+                count+=1;
+            n=n>>1;
+        }
+        return count;
+    }
+    bool isPowerOfTwo(int n) {
+        // if(n<0)
+        //     return false;
+        // if(countPwer(n)==1)
+        //     return true;
+        // else 
+        //     return false;
+        if(n==0) return false;
+        if(n==1) return true;
+        return n%2==0 && isPowerOfTwo(n/2);
+    }
+};
