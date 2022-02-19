e6f2a5ab8ffaaae4692ce941d51ac899a9ac0f90
Anica Singh <62740213+Anica8@users.noreply.github.com>
2022-02-19 17:17:06 +0530

    Time: 6 ms, Memory: 6.7 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> ans;
+        for(int i=0;i<=rowIndex;i++){
+            vector<int> a;
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i)
+                    a.push_back(1);
+                else{
+                    int m=ans[i-1][j-1]+ans[i-1][j];
+                    a.push_back(m);
+                }
+            }
+            ans.push_back(a);
+        }
+        return ans[rowIndex];
+    }
+};
