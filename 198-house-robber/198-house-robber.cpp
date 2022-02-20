dd15a7821b30de951898b5337e69f158bbff072c
Anica Singh <62740213+Anica8@users.noreply.github.com>
2022-02-20 19:33:41 +0530

    Time: 3 ms (52.03%), Space: 7.6 MB (95.07%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    int dp[105];
-    int houseRob(vector<int> n,int ind){
-        if(ind==0 || ind==1) return n[ind];
-        if(ind<0) return 0;
-        if(dp[ind]) return dp[ind];
-        return dp[ind]=max(houseRob(n,ind-2),houseRob(n,ind-3))+n[ind];
-    }
+    
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==0 || n==1) return nums[0];
