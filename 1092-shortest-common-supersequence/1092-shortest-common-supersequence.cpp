0ac6c70672192b4880297752b675ef3ca91c6b0c
Anica Singh <62740213+Anica8@users.noreply.github.com>
2022-03-18 23:00:47 +0530

    Time: 19 ms (60.35%), Space: 10.6 MB (80.04%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -57,9 +57,14 @@ public:
         //vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         int dp[n+1][m+1];
 
-        for(int j=0; j<=m; j++) dp[0][j]=0;
-        for(int i=0; i<=n; i++) dp[i][0]=0;
-
+        // for(int j=0; j<=m; j++) dp[0][j]=0;
+        // for(int i=0; i<=n; i++) dp[i][0]=0;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+            }
+        }
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 if(s[i-1]==t[j-1]) dp[i][j] = 1+dp[i-1][j-1];
